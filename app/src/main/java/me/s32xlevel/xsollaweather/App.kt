8cce99de361ab894617b24713724a7f8081e8227
arilkaev <xaysdiablo@gmail.com>
2019-11-16 00:43:54 +0500

    day 6 App class refactor

--- app/src/main/java/me/s32xlevel/xsollaweather/App.kt
+++ app/src/main/java/me/s32xlevel/xsollaweather/App.kt
@@ -1,11 +1,10 @@
 package me.s32xlevel.xsollaweather
 
 import android.app.Application
-import android.content.ContentValues
-import android.database.sqlite.SQLiteDatabase
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import me.s32xlevel.xsollaweather.util.DbUtils
 
 class App : Application() {
     private lateinit var database: AppDatabase
@@ -15,7 +14,6 @@ class App : Application() {
         fun getInstance() = instance as App
     }
 
-    // TODO onCreate Refactor
     override fun onCreate() {
         super.onCreate()
         instance = this
@@ -23,26 +21,7 @@ class App : Application() {
             .addCallback(object : RoomDatabase.Callback() {
                 override fun onCreate(db: SupportSQLiteDatabase) {
                     super.onCreate(db)
-                    db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
-                        put("id", 524901)
-                        put("name", "Москва")
-                        put("is_saved", true)
-                    })
-                    db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
-                        put("id", 511180)
-                        put("name", "Пермь")
-                        put("is_saved", true)
-                    })
-                    db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
-                        put("id", 498817)
-                        put("name", "Санкт-Петербург")
-                        put("is_saved", false)
-                    })
-                    db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
-                        put("id", 1496745)
-                        put("name", "Новосибирск")
-                        put("is_saved", false)
-                    })
+                    DbUtils.populateDb(db)
                 }
             })
             .allowMainThreadQueries()
--- app/src/main/java/me/s32xlevel/xsollaweather/util/DbUtils.kt
+++ app/src/main/java/me/s32xlevel/xsollaweather/util/DbUtils.kt
@@ -1,11 +1,37 @@
 package me.s32xlevel.xsollaweather.util
 
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import me.s32xlevel.xsollaweather.App
 import me.s32xlevel.xsollaweather.business.model.Weather
 import me.s32xlevel.xsollaweather.business.model.WeatherEntity
 import java.util.*
 
 object DbUtils {
+    fun populateDb(db: SupportSQLiteDatabase) {
+        db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
+            put("id", 524901)
+            put("name", "Москва")
+            put("is_saved", true)
+        })
+        db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
+            put("id", 511180)
+            put("name", "Пермь")
+            put("is_saved", true)
+        })
+        db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
+            put("id", 498817)
+            put("name", "Санкт-Петербург")
+            put("is_saved", false)
+        })
+        db.insert("cities", SQLiteDatabase.CONFLICT_NONE, ContentValues().apply {
+            put("id", 1496745)
+            put("name", "Новосибирск")
+            put("is_saved", false)
+        })
+    }
+
     fun saveWeatherToDb(pojoWeather: Weather) {
         val cityId = pojoWeather.city.id
         pojoWeather.list.forEach { weatherListItem ->
